941942928d6df4770380feac659968085a9583ba
Jyoti Bisht (Joe) <jyotibisht6114@gmail.com>
2021-10-18 21:58:43 +0530

    added view index.ejs

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const app = express();
 const recipeRouter = require("./routes/recipes")
 app.set("view engine", "ejs");
 
-app.use("/recipe",recipeRouter)
+app.use("/recipes",recipeRouter)
 
 app.get("/", (req, res)=>{
-    res.render("index", {text : "recipes"});
+    const recipes = [{
+        // we need a title, date and recipe
+        title:"Test recipe",
+        createdAt: new Date(),
+        ingre: "just checking"
+    },
+    {
+        // we need a title, date and recipe
+        title:"Test recipe 2",
+        createdAt: new Date(),
+        ingre: "just checking2"
+    }]
+    res.render("index", {recipes : recipes});
 })
 
 app.listen(3000);
