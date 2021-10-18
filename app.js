779861a77b574d497f01927215ca3d673b9c44d8
Jyoti Bisht (Joe) <jyotibisht6114@gmail.com>
2021-10-18 21:40:31 +0530

    added base route

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express();
-
+const recipeRouter = require("./routes/recipes")
 app.set("view engine", "ejs");
 
+app.use("/recipe",recipeRouter)
+
 app.get("/", (req, res)=>{
-    res.render("index");
+    res.render("index", {text : "recipes"});
 })
 
 app.listen(3000);
